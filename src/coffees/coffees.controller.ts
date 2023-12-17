e103bdcbaf50ae63da978b425e6e4099d725974e
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-17 22:18:32 +0700

    Basic Response Status Codes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get('flavors')
-    findAll() {
-        return 'This action returns all coffees';
+    // @Get('flavors') /coffees/flavors
+    @Get('') // /coffees
+    findAll(@Res() response) {
+        response.status(200).send('This action returns all coffees');
     }
     @Get(':id')
     findOne(@Param('id') id: string) {
         return `This actions return #${id} coffee`
     }
     @Post()
+    @HttpCode(HttpStatus.GONE)
     create(@Body() body) {
         return body
     }
