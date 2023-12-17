166ec5083e45cb65538f9c60f59f83a71ce22c06
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-17 22:25:10 +0700

    Basic Handing Update and Delete Requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
-import { response } from 'express';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
     // @Get('flavors') /coffees/flavors
-    @Get('') // /coffees
-    findAll(@Res() response) {
-        response.status(200).send('This action returns all coffees');
+    @Get()
+    findAll() {
+        return 'This action returns all coffees';
     }
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `This actions return #${id} coffee`
+        return `This action return #${id} coffee`;
     }
     @Post()
-    @HttpCode(HttpStatus.GONE)
+    // @HttpCode(HttpStatus.GONE)
     create(@Body() body) {
-        return body
+        return body;
+    }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return `This action updates #${id} coffee`;
+    }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes #${id} coffee`;
     }
 }
