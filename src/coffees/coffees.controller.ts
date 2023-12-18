e11746ef0b2185dde106c8cdcdfa822ed58fd42e
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-18 22:46:06 +0700

    Introduction to Data Transfer Objects

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,29 +1,30 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
     constructor(private readonly coffeesService: CoffeesService) { }
 
-    @Get() // @Get('flavors') /coffees/flavors
-    findAll(@Query() paginationQuery) {
-        const { limit, offset } = paginationQuery;
-        return this.coffeesService.findAll(); // return `This action returns all coffees Limit: ${limit} , offset: ${offset}`;
+    @Get()
+    findAll() {
+        return this.coffeesService.findAll();
     }
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return this.coffeesService.findOne(id); // return `This action return #${id} coffee`;
+        return this.coffeesService.findOne(id);
     }
-    @Post() // @HttpCode(HttpStatus.GONE) optional!
-    create(@Body() body) {
-        return this.coffeesService.create(body); // return body;
+    @Post()
+    create(@Body() createCoffeeDto: CreateCoffeeDto) {
+        return this.coffeesService.create(createCoffeeDto);
     }
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body) {
-        return this.coffeesService.update(id, body) // return `This action updates #${id} coffee`; 
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+        return this.coffeesService.update(id, updateCoffeeDto);
     }
     @Delete(':id')
     remove(@Param('id') id: string) {
-        return this.coffeesService.remove(id) // return `This action removes #${id} coffee`;
+        return this.coffeesService.remove(id);
     }
 }
