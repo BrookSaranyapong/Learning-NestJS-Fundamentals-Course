1ee6bf045154929f0fc5191e9499958369dac462
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-20 20:12:34 +0700

    Using Repository to Access Database

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -8,8 +8,7 @@ export class CoffeesController {
     constructor(private readonly coffeesService: CoffeesService) { }
 
     @Get()
-    findAll(@Query() paginationQuery) {
-        // const { limit, offset } = paginationQuery;
+    findAll() {
         return this.coffeesService.findAll();
     }
     @Get(':id')
