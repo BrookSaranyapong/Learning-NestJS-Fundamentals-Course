84d419bc3ad810ad023597586d8e2f844489ab7b
Brooksaranyapong <brook.saranyapong@gmail.com>
2024-01-14 19:49:11 +0700

    Introducing the Swagger Module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,15 @@ async function bootstrap() {
       },
     }),
   );
-  app.useGlobalInterceptors(new TimeoutInterceptor());
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  // app.useGlobalInterceptors(new TimeoutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
